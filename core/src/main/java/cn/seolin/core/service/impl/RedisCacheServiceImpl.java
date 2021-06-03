de48a41ae9e35092a63d08f2f053f6fbef07c176
kkchen <liangzi.ckk@raycloud.com>
2021-06-03 21:50:08 +0800

    增加redisson的简单使用

--- core/src/main/java/cn/seolin/core/service/impl/RedisCacheServiceImpl.java
+++ core/src/main/java/cn/seolin/core/service/impl/RedisCacheServiceImpl.java
@@ -1,23 +1,35 @@
 package cn.seolin.core.service.impl;
 
 import cn.seolin.core.service.CacheService;
+import org.redisson.Redisson;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author liangzi on 2021/6/3
  */
 public class RedisCacheServiceImpl implements CacheService {
+
+    private RedissonClient redissonClient;
+
     @Override
     public void addCache(String key, String value, long time) {
-
+        RBucket<Object> bucket = redissonClient.getBucket(key);
+        bucket.set(value, time, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public String getCache(String key) {
-        return null;
+        return redissonClient.getBucket(key).get().toString();
     }
 
     @Override
     public void init() {
-
+        //TODO:从配置文件中取创建方法
+        Config config = new Config();
+        redissonClient = Redisson.create(config);
     }
 }
